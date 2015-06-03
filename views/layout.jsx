8e46ee45c8b137c0f9dc73904fb6a9529de567e7
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-02 21:37:29 -0300

    modificado el layout para agregar font-awesome y la fuente roboto

--- views/layout.jsx
+++ views/layout.jsx
@@ -8,7 +8,9 @@ const Layout = React.createClass({
           <meta charser="utf-8" />
           <title>React-TODO</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" />
           <link rel="stylesheet" href="/assets/css/style.css" />
+          <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto" />
         </head>
         <body>
           <main role="application" id="app">
