4da2d253aa9db4efc7c894b09d9a0a89eab4c2bf
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-03 02:39:23 -0300

    terminado el componente de todo

--- components/todo.jsx
+++ components/todo.jsx
@@ -1,51 +1,113 @@
 import React from 'react';
+import xhr   from 'promised-xhr';
 
 const Todo = React.createClass({
   getInitialState() {
-    return this.props;
+    const { _id, status, todo } = this.props;
+
+    return { _id, status, todo };
   },
 
-  updateStatus() {},
-  changeTodo() {},
-  deleteTodo() {},
+  updateStatus: async function (e) {
+    try {
+      e.preventDefault();
 
-  onChange(e) {
-    console.log(e);
+      let status;
+
+      if (this.state.status) status = false;
+      else status = 'on';
+
+      const state = {
+        _id: this.state._id,
+        todo: this.state.todo,
+        status: status
+      };
+
+      this.setState(state);
+
+      const response = await xhr.send('/api', {
+        method: 'PUT',
+        data  : state
+      });
+    } catch (err) {
+      return err;
+    }
+  },
+
+  changeTodo: async function (e) {
+    try {
+      e.preventDefault();
+
+      const response = await xhr.send('/api', {
+        method: 'PUT',
+        data  : this.state
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  deleteTodo: async function (e) {
+    try {
+      e.preventDefault();
+
+      const { body: todos } = await xhr.send('/api', {
+        method: 'DELETE',
+        data: this.state
+      });
+
+      this.props.removedTodo(todos);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  onChangeTodo: async function (e) {
+    try {
+      const todo = e.target.value;
+
+      this.setState({
+        _id: this.state._id,
+        status: this.state.status,
+        todo
+      });
+    } catch (err) {
+      console.log(err);
+    }
   },
 
   render() {
     return (
       <article>
-        <form action="/todo"
-         method="PUT"
-         className="form-change-status">
-          <input type="hidden"
+        <form onSubmit={ this.changeTodo }>
+          <input type="hidden" ref="id"
            name="_id" value={ this.state._id} />
-          <input type="hidden"
-           name="todo" value={ this.state.todo} />
-          <input type="checkbox" onChange={ this.onChange }
-           name="status" checked={ this.state.checked} />
-          <button></button>
-        </form>
 
-        <form action="/todo"
-         method="PUT"
-         className="form-change-todo">
-          <input type="hidden"
-           name="_id" value={ this.state._id} />
-          <input type="hidden"
-           name="status" value={ this.state.checked} />
-          <input type="text"
-           name="todo" value={ this.state.todo} />
-          <button className="fa fa-pencil"></button>
-        </form>
+          <div className="form-change-status">
+            <input type="checkbox"
+             ref="status"
+             name="status"
+             checked={ this.state.status} />
+            <button type="button"
+             onClick={ this.updateStatus }></button>
+          </div>
 
-        <form action="/todo"
-         method="DELETE"
-         className="form-delete-todo">
-          <input type="hidden"
-           name="_id" value={ this.state._id} />
-          <button className="fa fa-close"></button>
+          <div className="form-change-todo">
+            <input type="text"
+             onChange={ this.onChangeTodo }
+             ref="name"
+             name="todo"
+             value={ this.state.todo} />
+            <button type="button"
+             onClick={ this.changeTodo}
+             className="fa fa-pencil"></button>
+          </div>
+
+          <div className="form-delete-todo">
+            <button type="button"
+             onClick={ this.deleteTodo }
+             className="fa fa-close"></button>
+          </div>
         </form>
       </article>
     );
