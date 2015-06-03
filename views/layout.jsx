0fe32ef3d89eec1377e2531ebeebb532be89253a
Sergio Xalambrí <sergio@xalambri.com.ar>
2015-06-03 03:30:07 -0300

    agregado ribbon de fork me on github

--- views/layout.jsx
+++ views/layout.jsx
@@ -9,6 +9,7 @@ const Layout = React.createClass({
           <title>React-TODO</title>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" />
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.1.1/gh-fork-ribbon.min.css" />
           <link rel="stylesheet" href="/assets/css/style.css" />
           <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto" />
         </head>
@@ -16,6 +17,14 @@ const Layout = React.createClass({
           <main role="application" id="app">
             { this.props.children }
           </main>
+
+          <div className="github-fork-ribbon-wrapper left-bottom">
+            <div className="github-fork-ribbon">
+              <a href="//github.com/sergiodxa/isomorphic-react-todo" target="_blank">
+                Fork me on GitHub
+              </a>
+            </div>
+          </div>
 
           <script src="/assets/js/app.js"></script>
         </body>
